903eaf41d21c069250b21429dd495095bb8b2146
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-03 10:11:01 -0500

    Create TagRepository.java

---
+++ src/main/java/com/tts/demoTwitter/repository/TagRepository.java
@@ -0,0 +1,10 @@
+package com.tts.demoTwitter.repository;
+
+import com.tts.demoTwitter.model.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+    Tag findByPhrase(String phrase);
+}
