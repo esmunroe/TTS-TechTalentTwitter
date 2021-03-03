d4a4528f5545610faf39168e6c4e863f1915f5a4
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-03 10:11:16 -0500

    Update TweetService.java

--- src/main/java/com/tts/demoTwitter/service/TweetService.java
+++ src/main/java/com/tts/demoTwitter/service/TweetService.java
@@ -1,10 +1,20 @@
 package com.tts.demoTwitter.service;
 
-import com.tts.demoTwitter.model.Tweet;
-import com.tts.demoTwitter.model.User;
-import com.tts.demoTwitter.repository.TweetRepository;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tts.demoTwitter.model.Tag;
+import com.tts.demoTwitter.model.Tweet;
+import com.tts.demoTwitter.model.User;
+import com.tts.demoTwitter.repository.TagRepository;
+import com.tts.demoTwitter.repository.TweetRepository;
 
 import java.util.List;
 
@@ -14,9 +24,12 @@ public class TweetService {
     @Autowired
     private TweetRepository tweetRepository;
 
+    @Autowired
+    private TagRepository tagRepository;
+
     public List<Tweet> findAll() {
         List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
-        return tweets;
+        return formatTweets(tweets);
     }
 
     public List<Tweet> findAllByUser(User user) {
@@ -24,12 +37,77 @@ public class TweetService {
         return tweets;
     }
 
-    public List<Tweet> findAllByUsers(List<User> users){
+    public List<Tweet> findAllByUsers(List<User> users) {
         List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
         return tweets;
     }
 
+    public List<Tweet> findAllWithTag(String tag) {
+        List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(tag);
+        return formatTweets(tweets);
+    }
+
     public void save(Tweet tweet) {
+        handleTags(tweet);
         tweetRepository.save(tweet);
+    }
+
+    private void handleTags(Tweet tweet) {
+        List<Tag> tags = new ArrayList<Tag>();
+        Pattern pattern = Pattern.compile("#\\w+");
+        Matcher matcher = pattern.matcher(tweet.getMessage());
+        while (matcher.find()) {
+            String phrase = matcher.group().substring(1).toLowerCase();
+            Tag tag = tagRepository.findByPhrase(phrase);
+            if (tag == null) {
+                tag = new Tag();
+                tag.setPhrase(phrase);
+                tagRepository.save(tag);
+            }
+            tags.add(tag);
+        }
+        tweet.setTags(tags);
+    }
+
+    private List<Tweet> formatTweets(List<Tweet> tweets) {
+        addTagLinks(tweets);
+        shortenLinks(tweets);
+        return tweets;
+    }
+
+    private void addTagLinks(List<Tweet> tweets) {
+        Pattern pattern = Pattern.compile("#\\w+");
+        for (Tweet tweet : tweets) {
+            String message = tweet.getMessage();
+            Matcher matcher = pattern.matcher(message);
+            Set<String> tags = new HashSet<String>();
+            while (matcher.find()) {
+                tags.add(matcher.group());
+            }
+            for (String tag : tags) {
+                message = message.replaceAll(tag,
+                        "<a class=\"tag\" href=\"/tweets/" + tag.substring(1).toLowerCase() + "\">" + tag + "</a>");
+            }
+            tweet.setMessage(message);
+        }
+    }
+
+    private void shortenLinks(List<Tweet> tweets) {
+        Pattern pattern = Pattern.compile("https?[^ ]+");
+        for (Tweet tweet : tweets) {
+            String message = tweet.getMessage();
+            Matcher matcher = pattern.matcher(message);
+            while (matcher.find()) {
+                String link = matcher.group();
+                String shortenedLink = link;
+                if (link.length() > 23) {
+                    shortenedLink = link.substring(0, 20) + "...";
+                    message = message.replace(link,
+                            "<a class=\"tag\" href=\"" + link + "\" target=\"_blank\">" + shortenedLink + "</a>");
+                }
+                tweet.setMessage(message);
+            }
+
+        }
     }
 }
