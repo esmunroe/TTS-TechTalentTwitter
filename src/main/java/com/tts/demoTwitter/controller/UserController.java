ed78a81972fc7724f3606948bda570caca46313a
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-04 09:51:33 -0500

    User page tweet filtering

--- src/main/java/com/tts/demoTwitter/controller/UserController.java
+++ src/main/java/com/tts/demoTwitter/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,24 +25,33 @@ public class UserController {
     @Autowired
     private TweetService tweetService;
 
-    @GetMapping(value = "/users/{username}")
-    public String getUser(@PathVariable(value = "username") String username, Model model) {
+    @GetMapping(value = "/users")
+    public String getUsers(@RequestParam(value = "filter", required = false) String filter, Model model) {
+        List<User> users = new ArrayList<User>();
+
         User loggedInUser = userService.getLoggedInUser();
-        User user = userService.findByUsername(username);
-        List<TweetDisplay> tweets = tweetService.findAllByUser(user);
-        List<User> following = loggedInUser.getFollowing();
-        boolean isFollowing = false;
-        for (User followedUser : following) {
-            if (followedUser.getUsername().equals(username)) {
-                isFollowing = true;
-            }
+
+        List<User> usersFollowing = loggedInUser.getFollowing();
+        List<User> usersFollowers = loggedInUser.getFollowers();
+        if (filter == null) {
+            filter = "all";
         }
-        boolean isSelfPage = loggedInUser.getUsername().equals(username);
-        model.addAttribute("tweetList", tweets);
-        model.addAttribute("user", user);
-        model.addAttribute("following", isFollowing);
-        model.addAttribute("isSelfPage", isSelfPage);
-        return "user";
+        if (filter.equalsIgnoreCase("followers")) {
+            users = usersFollowers;
+            model.addAttribute("filter", "followers");
+        } else if (filter.equalsIgnoreCase("following")) {
+            users = usersFollowing;
+            model.addAttribute("filter", "following");
+        } else {
+            users = userService.findAll();
+            model.addAttribute("filter", "all");
+        }
+        model.addAttribute("users", users);
+
+        SetTweetCounts(users, model);
+        SetFollowingStatus(users, usersFollowing, model);
+
+        return "users";
     }
 
     @GetMapping(value = "/users")
